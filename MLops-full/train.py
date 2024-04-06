e86c04da892a9e1dc53cc3f535996c11dc862993
Nefta_Camfig <100545486+NEFTACAMFIG@users.noreply.github.com>
2024-04-06 06:56:05 +0200

    Create train.py

---
+++ MLops-full/train.py
@@ -0,0 +1,8 @@
+# script para entrenar usando MLFLOW
+import argparse
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
